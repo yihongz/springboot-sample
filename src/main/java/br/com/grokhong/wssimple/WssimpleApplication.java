d86d5ff8c701c1948596f5a931420085de0ff56e
Paulo Almeida <estpaulovictora@live.com>
2021-06-19 20:15:10 -0300

    Project created

---
+++ src/main/java/br/com/grokhong/wssimple/WssimpleApplication.java
@@ -0,0 +1,13 @@
+package br.com.grokhong.wssimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WssimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WssimpleApplication.class, args);
+	}
+
+}
