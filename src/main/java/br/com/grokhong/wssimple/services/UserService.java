d2a0051dff38bfb123d4940897671428dd121af1
Paulo Almeida <estpaulovictora@live.com>
2021-06-19 22:09:32 -0300

    Service layer, component registration

---
+++ src/main/java/br/com/grokhong/wssimple/services/UserService.java
@@ -0,0 +1,26 @@
+package br.com.grokhong.wssimple.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.grokhong.wssimple.entities.User;
+import br.com.grokhong.wssimple.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = Optional.of(repository.getById(id));
+		return obj.get();
+	}
+}
