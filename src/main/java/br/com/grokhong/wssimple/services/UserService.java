db95016df3fec65cde30a16c37656f2fb33dae4a
Paulo Almeida <estpaulovictora@live.com>
2021-06-20 16:15:43 -0300

    User insert

--- src/main/java/br/com/grokhong/wssimple/services/UserService.java
+++ src/main/java/br/com/grokhong/wssimple/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
 		Optional<User> obj = Optional.of(repository.getById(id));
 		return obj.get();
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
 }
