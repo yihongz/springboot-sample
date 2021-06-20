f827c4f9a9c2b8934dc3121e16f7970635bbc28c
Paulo Almeida <estpaulovictora@live.com>
2021-06-20 16:01:06 -0300

    Payment, one-to-one association

--- src/main/java/br/com/grokhong/wssimple/config/TestConfig.java
+++ src/main/java/br/com/grokhong/wssimple/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.grokhong.wssimple.entities.Category;
 import br.com.grokhong.wssimple.entities.Order;
 import br.com.grokhong.wssimple.entities.OrderItem;
+import br.com.grokhong.wssimple.entities.Payment;
 import br.com.grokhong.wssimple.entities.Product;
 import br.com.grokhong.wssimple.entities.User;
 import br.com.grokhong.wssimple.entities.enums.OrderStatus;
@@ -78,5 +79,10 @@ public class TestConfig implements CommandLineRunner {
 			OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 			orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+			Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+			o1.setPayment(pay1);
+			
+			orderRepository.save(o1);
 		}
 }
