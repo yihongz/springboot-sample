8d1cd5fd283daef05425fcf56655a12534d5ae1b
Paulo Almeida <estpaulovictora@live.com>
2021-06-20 21:30:14 -0300

    Exception handling - findById unfinished

--- src/main/java/br/com/grokhong/wssimple/services/UserService.java
+++ src/main/java/br/com/grokhong/wssimple/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.grokhong.wssimple.entities.User;
 import br.com.grokhong.wssimple.repositories.UserRepository;
+import br.com.grokhong.wssimple.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = Optional.of(repository.getById(id));
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
