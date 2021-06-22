57c7350afabab734a118a08df0e51119285a6d5b
Paulo Almeida <estpaulovictora@live.com>
2021-06-22 00:51:08 -0300

    Exception handling - update

--- src/main/java/br/com/grokhong/wssimple/services/UserService.java
+++ src/main/java/br/com/grokhong/wssimple/services/UserService.java
@@ -3,6 +3,8 @@ package br.com.grokhong.wssimple.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -45,9 +47,14 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getById(id);
-		updatedData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getById(id);
+			updatedData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updatedData(User entity, User obj) {
